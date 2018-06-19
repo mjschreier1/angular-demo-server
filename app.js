0f157603174763b4ab73643e156c912c5780b4e8
Michael Schreier <mjschreier1@gmail.com>
2018-06-19 12:16:44 -0600

    list component works

--- app.js
+++ app.js
@@ -11,14 +11,19 @@ app.use(bodyParser.json());
 const data = [
     {
         id: 1,
-        item: "Milk",
+        name: "Milk",
         quantity: 1
     },
     {
         id: 2,
-        item: "Bread",
+        name: "Bread",
         quantity: 2,
         notes: "Need a loaf of white and a loaf of wheat to satisfy the picky eaters."
+    },
+    {
+        id: 3,
+        name: "Hand Soap",
+        notes: "The master bath and kitchen are both out."
     }
 ]
 
@@ -36,13 +41,21 @@ app.post("/list", (req, res) => {
 
 app.delete("/list", (req, res) => {
     let itemDeleted = data.reduce((acc, item, i) => {
-        if (item.id === req.body.id) acc.push(data.splice(i, 1));
+        if (item.id === parseInt(req.query.id)) acc = data.splice(i, 1);
         return acc;
     }, [])
-    res.send({
-        error: false,
-        itemDeleted: itemDeleted[0]
-    })
+    if (itemDeleted[0]) {
+        res.send({
+            error: false,
+            itemDeleted: itemDeleted[0]
+        })
+    } else {
+        res.status(404);
+        res.send({
+            error: true,
+            itemDeleted: null
+        })
+    }
 })
 
 app.listen(port, () => console.log(`App started on port ${port}`))
